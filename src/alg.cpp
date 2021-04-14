29482ebefb4d7b37320adf61d86495f059718b12
DivineSunderer <78905175+DivineSunderer@users.noreply.github.com>
2021-04-14 21:10:39 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,70 +1,62 @@
 // Copyright 2021 NNTU-CS
 
 int countPairs1(int *arr, int len, int value) {
-  return 0;
-}
-        int count = 0;
-                for (int i = 0; i < len-1; i++) {
-                for (int j = i + 1; j < len; j++) {
-        if (arr[i] + arr[j] == value)
+    int count = 0;
+    for (int i = 0; i < (len - 1); i++) 
+        for (int j = i + 1; j < len; j++) 
+    for (int i = 0; i < (len - 1); i++) {
+        for (int j = i + 1; j < len; j++) {
+            if ((arr[i] + arr[j]) == value) {
                 count++;
+            }
         }
-        }
-        if ( count > 0 )
-                return count;
-        else
-                return 0;
-        }
-
+    }
+    return count;
+}
 int countPairs2(int *arr, int len, int value) {
-  return 0;
-}
-        int count = 0;
-        int j = len - 1;
-                while (arr[j] > value) {
-        j--;
-        }
-                for (j; j > 0; j--) {
-                for (int i = 0; i < j; i++) {
-                if ((arr[i] + arr[j]) == value)
+    int count = 0, num = len - 1;
+    while ((arr[0] + arr[num]) > value) {
+        num--;
+    }
+    for (int i = 0; i < (num); i++) {
+        for (int j = i + 1; j < (num + 1); j++) {
+            if ((arr[i] + arr[j]) == value) {
                 count++;
+            }
         }
-        }
-        if ( count > 0 )
-                return count;
-        else
-                return 0;
-        }
-
-
-int countPairs3(int *arr, int len, int value) {
-  return 0;
+    }
+    return count;
 }
-        int count = 0;
-                for (int i = 0; i < len-1; i++) {
-        int left = i+1;
-        int right = len;
-        int res = value - arr[i];
-                while (left < right) {
-        int center = (left + right)/2;
-                if (arr[center] == res) {
-                while ((arr[center-1] == res) && (center > left)) {
-        center--;
+int countPairs3(int *arr, int len, int value) {
+    int count = 0, lborder = 0, rborder = len;
+    int med = (lborder + rborder) / 2, left = med, right = med;
+    for (int i = 0; i < len; i++) {
+        while (rborder >= lborder) {
+            med = (lborder + rborder) / 2;
+            left = med - 1;
+            right = med + 1;
+            if (arr[i] + arr[med] == value) {
+                if (i != med) {
+                    count++;
+                }
+                while (((arr[i] + arr[left]) == value) && (i != left)) {
+                    count++;
+                    left--;
+                }
+                while (((arr[i] + arr[right]) == value) && (i != right)) {
+                    count++;
+                    right++;
+                }
+                rborder = lborder - 1;
+                } else if ((arr[i] + arr[med]) < value) {
+                        lborder = med + 1;
+                    } else if ((arr[i] + arr[med]) > value) {
+                                rborder = med - 1;
+                            }
         }
-                while (arr[center] == res) {
-        center++;
-        count++;
-        }
-        break;
-        } else if (arr[center] > res) {
-        right = center;
-        } else {
-        left = center+1;
-        }
-        }
-        }
-        if ( count > 0 )
-                return count;
-        else
-                return 0;
-        }
+        arr[i] = value + 1;
+        rborder = len;
+        lborder = i + 1;
+    }
+    return count;
+}
